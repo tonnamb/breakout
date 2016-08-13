632875bf4e056072f0c0002534825b608c777a0a
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-13 09:25:23 -0400

    clearing canvas

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var dx = 2;
 var dy = -2;
 
 function draw() {
-    // drawing code
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
