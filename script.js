0dd90f8cff220ec9dc36655d55d880b3e514f6a8
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-13 09:18:45 -0400

    move the ball

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
 function draw() {
     // drawing code
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+    x += dx;
+    y += dy;
 }
 
 setInterval(draw, 10); // call draw() every 10 milliseconds forever
