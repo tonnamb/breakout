69ef3ad29b1f7bc702da8b99695830c55dd44fbb
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-13 10:56:35 -0400

    draw Bricks

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var brick = [];
+for (var c = 0; c < brickColumnCount; c += 1) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r += 1) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -34,6 +50,22 @@ function drawPaddle() {
 
 function randomColor() {
     return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c += 1) {
+        for (var r = 0; r < brickRowCount; r += 1) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
 }
 
 function draw() {
@@ -72,6 +104,9 @@ function draw() {
     } else if (leftPressed && paddleX > 0) {
         paddleX -= 7; 
     }
+
+    // ****** Bricks ******
+    drawBricks();
 }
 
 function keyDownHandler(e) {
