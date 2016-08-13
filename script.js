5268547ca13d357f2581e9d7926bbd884fb258a5
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-08-13 09:14:47 -0400

    call draw() every 10 ms forever

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function draw() {
+    // drawing code
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+setInterval(draw, 10); // call draw() every 10 milliseconds forever
